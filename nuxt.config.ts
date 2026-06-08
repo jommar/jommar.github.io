c18b8e21458444a1d084dae07e4205d3e87c0627
Jommar Ilagan <jom102386@gmail.com>
2026-06-09 07:48:19 +0800

    Disable Nuxt Devtools to prevent favicon override

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   srcDir: ".",
   css: ["~/assets/styles/main.css"],
 
